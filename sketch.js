d82f8b50f7e083464dc537bad906d5eb76a66c8c
n0nbinarycode <106261653+n0nbinarycode@users.noreply.github.com>
2023-05-15 18:21:45 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -9,7 +9,7 @@ function setup() {
 
 function draw() {
   background(220);
-  fill(random(255), random(255, random(255));
+  fill(random(255), random(255), random(255));
   ellipse(x, y, 50, 50);
   ellipse(10, y, 50, 50);
   x=x+1;
