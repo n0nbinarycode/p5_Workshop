5a37953ea96a1e392ee14953f27bf2841c549752
n0nbinarycode <106261653+n0nbinarycode@users.noreply.github.com>
2023-05-15 17:59:14 +0100

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,8 +1,14 @@
+let x;
+let y;
+
 function setup() {
   createCanvas(400, 400);
+  x = width/2;
+  y = height/2;
 }
 
 function draw() {
   background(220);
   ellipse(200, 200, 50, 50);
+  x++;
 }
